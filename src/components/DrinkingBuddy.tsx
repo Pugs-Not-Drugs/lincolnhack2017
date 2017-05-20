a9c826bf47fe202d1aa8bd41fd6e0040f854a237
Stephen Brown <stephen@codemake.rs>
2017-05-20 23:38:17 +0100

    banners

--- src/components/DrinkingBuddy.tsx
+++ src/components/DrinkingBuddy.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { gameStateStore } from "../stores";
 import { rowComplete, columnComplete, GameStateEventResolver } from "../GameStateEventResolver"
+import { BuddyWithPhrase } from "../models/BuddyWithPhrase";
 
 export interface EventProps { }
 
@@ -8,8 +9,10 @@ export class DrinkingBuddy extends React.Component<EventProps,{}> {
     imageUrl: string;
     winUrl: string
     timerId: NodeJS.Timer;
-    images: string[]
+    images: BuddyWithPhrase[]
     animations: string[]
+    bannerClass: string
+    bannerText: string
 
     constructor() {
         super();
@@ -17,13 +20,13 @@ export class DrinkingBuddy extends React.Component<EventProps,{}> {
         this.imageUrl = "";
         this.winUrl = "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/db.png";
         this.images = [
-            "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/1.png",
-            "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/2.png",
-            "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/3.png",
-            "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/4.png",
-            "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/5.png",
-            "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/6.png",
-            "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/7.png",
+            new BuddyWithPhrase("Strong and Stable", "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/1.png", "conservative"),
+            new BuddyWithPhrase("For the many, not the few", "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/2.png", "labour"),
+            new BuddyWithPhrase("#IAgreeWithTim", "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/3.png", "libdems"),
+            new BuddyWithPhrase("Freedom", "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/4.png", "snp"),
+            new BuddyWithPhrase("Green Guarantee", "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/5.png", "greens"),
+            new BuddyWithPhrase("Tarian Cymru", "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/6.png", "plaidcymru"),
+            new BuddyWithPhrase("Zero Net Immigration", "https://s3-eu-west-1.amazonaws.com/gebingo.co.uk/Politicians/7.png", "ukip"),
         ]
         this.animations = [
             "animateOne",
@@ -35,15 +38,18 @@ export class DrinkingBuddy extends React.Component<EventProps,{}> {
 
     handleStateChange() {
         var state = gameStateStore.getState();
-        if(rowComplete(state.game, 0) && rowComplete(state.game, 1) && rowComplete(state.game, 2) && rowComplete(state.game, 3) && rowComplete(state.game, 4)) {
+        /*if(rowComplete(state.game, 0) && rowComplete(state.game, 1) && rowComplete(state.game, 2) && rowComplete(state.game, 3) && rowComplete(state.game, 4)) {
             this.imageUrl = this.winUrl;
             clearInterval(this.timerId);
             this.timerId = setInterval(() => this.tick(), 4000);
             this.setState({
                 newForm : true
             });
-        } else if(rowComplete(state.game, state.lastSquareActioned.x) || columnComplete(state.game, state.lastSquareActioned.y)) { 
-            this.imageUrl = this.images[Math.floor(Math.random() * this.images.length)];
+        } else */if(rowComplete(state.game, state.lastSquareActioned.x) || columnComplete(state.game, state.lastSquareActioned.y)) { 
+            let selectedBuddy: BuddyWithPhrase = this.images[Math.floor(Math.random() * this.images.length)];
+            this.imageUrl = selectedBuddy.image;
+            this.bannerClass = selectedBuddy.bannerClass
+            this.bannerText = selectedBuddy.phrase
             clearInterval(this.timerId);
             this.timerId = setInterval(() => this.tick(), 4000);
             this.setState({
@@ -62,7 +68,10 @@ export class DrinkingBuddy extends React.Component<EventProps,{}> {
 
   render() {
         return (
-            <div id="drinkingBuddy" className={(this.imageUrl == "" ? "" : this.animations[Math.floor(Math.random() * this.animations.length)])}><img src={this.imageUrl}/></div>
+            <div>
+                <div className={(this.imageUrl ? "banner " : "") + this.bannerClass}>{this.bannerText}</div>
+                <div id="drinkingBuddy" className={(this.imageUrl == "" ? "" : this.animations[Math.floor(Math.random() * this.animations.length)])}><img src={this.imageUrl}/></div>
+            </div>
         );
   }
 }
---
+++ src/models/BuddyWithPhrase.tsx
@@ -0,0 +1,11 @@
+export class BuddyWithPhrase {
+	phrase: string;
+	image: string;
+	bannerClass: string
+
+	constructor(phrase: string, image: string, bannerClass: string) {
+		this.phrase = phrase;
+		this.image = image;
+		this.bannerClass = bannerClass;
+	}
+}
